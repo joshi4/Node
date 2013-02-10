16cf2925438fa1fca079657c30f8686fef8ef2db
Shantanu <joshishantanu4@gmail.com>
2013-02-10 15:48:50 -0500

    Creating a blog as practice for Senior Design

--- blog/app.js
+++ blog/app.js
@@ -1,69 +1,101 @@
-var http = require('http'); 
-var url = require('url'); 
-var fs = require('fs'); 
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'); 
+/*  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');*/
+
+var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
+
+var app = express();
+
+app.configure(function(){
+ // app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  //app.use(express.favicon());
+  //app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static((__dirname+ '/public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
 
 
-//new.html located in views/Posts/new.html
+var articleProvider= new ArticleProvider();
 
 
-var newPostFormHTML = fs.readFileSync('views/Posts/new.html'); 
-
-function renderNewPostForm( request, response)
-{
-
-	response.writeHead('200', 
-	{ 
-
-		'Content-type' : 'text/html; charset=utf-8'
-
-	}); 
-
-	//response.write("HelloWorld"); 
-
-	response.end(newPostFormHTML); 
+app.get('/', function(req, res){
+    articleProvider.findAll( function(error,docs){
+        res.render('index.jade', {
+            title: 'Blog',
+            articles:docs
+            }
+        );
+    })
+})
 
 
-} ; 
+app.get('/blog/new', function(req, res) {
+    res.render('blog_new.jade', {
+        title: 'New Post'
+    }
+    );
+});
 
-
-function render404page(request, response)
-{
-
-	response.writeHead(404); //send back error code in the header. 
-
-
-	//response.write("HelloWorld"); 
-
-	response.end('Error! 404 Page not found.'); 
-
-
-}
-
-
-var server = http.createServer(function (request, response) {
-
-var newPostFormRegExp = new RegExp('^/posts/new/?$'); 
-var pathname = url.parse(request.url).pathname; 
-
-//now we test to see whether the new post url has been requested.
-
-//this part should probably go in the router. 
-
-if (newPostFormRegExp.test(pathname)) 
-	{
-		renderNewPostForm(request, response); 
-	} 
-else
-	{
-		render404page(request, response); 
-	}
+app.post('/blog/new', function(req, res){
+    articleProvider.save({
+        title: req.param('title'),
+        body: req.param('body')
+    }, function( error, docs) {
+        res.redirect('/')
+    });
+});
 
 
 
-}); 
+/*app.get('/', function(req,res){
+  articleProvider.findAll(function(error,docs){
 
-server.listen(8888); 
+    res.send(docs); 
+  }); 
+  })*/
 
-console.log('listening to http://127.0.0.1 on port 8888'); 
+
+app.listen(3000) ; 
 
 
+
+
+
+
+
+
+
+
+
+
+
+//app.get('/users', user.list);
+
+
+
+
+
+
+
+
+
+
+/*http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});*/
