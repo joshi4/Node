42a39ca5a9060c1b4e477a692ddf506dbb12ffdc
Shantanu <joshishantanu4@gmail.com>
2013-02-17 23:39:18 -0500

    tested wifi simulation successfully

--- vitalis/app.js
+++ vitalis/app.js
@@ -64,11 +64,11 @@ var patientSchema = new mongoose.Schema({
   doctor: String,
   gender: String,
   patientId: String,
-  pulse: String,
-  oxygen: String,
-  temp: String,
+  pulse: [Number],
+  oxygen: [Number],
+  temp: [Number],
   fall: Number,
-  Alarm: Number}); 
+  Alarm: Number});  
 
   /*Explanation:
     1. name,gender: Self explanatory
@@ -116,6 +116,18 @@ app.get('/new/patient', function(req,res){
   });
 
 
+app.get('/:username/patient/:patientname',function(req,res){
+  patientInfo.find({
+    doctor: req.params.username, 
+    name: req.params.patientname
+  }, function(err,docs){
+    console.log(docs); 
+    res.render('patients/patientinfo', {patients: docs[0]}); 
+    // also make post requests, based on unique id. 
+  });
+});
+
+
 //POST REQUEST TO CREATE A NEW PATIENT. 
 app.post('/new/patient/?', function(req,res){
   var b = req.body 
@@ -124,11 +136,11 @@ app.post('/new/patient/?', function(req,res){
   doctor: b.doctor,
   gender: b.gender,
   patientId: b.patientId,
-  pulse: "",
-  oxygen: "",
-  temp: "",
-  fall: "",
-  Alarm: ""}).save(function(err,patient){
+  pulse: [0],
+  oxygen: [0],
+  temp: [0],
+  fall: 0,
+  Alarm: 0}).save(function(err,patient){
     if(err) res.json(err); 
     res.redirect('/users/' + b.doctor); 
   });
@@ -140,7 +152,15 @@ app.post('/new/patient/?', function(req,res){
 app.post('/login',function(req,res){
   var b = req.body ; 
   loginInfo.find({username: b.username},function(err,user){ 
-      if(user[0].username != b.username || user[0].password != b.password || err) {
+      console.log(user);
+      if (err){
+        res.redirect('/login'); 
+      } 
+      else if (user === [{}]) {
+        res.redirect('/login'); 
+        //return handleError(err); 
+      }
+      else if(user[0].username != b.username || user[0].password != b.password || err) {
         console.log("DBusername = " + user[0].username + "Requsername = " + b.username); 
         console.log("DBpass = " + user[0].password + "Reqpassword = " + b.password); 
         
@@ -154,6 +174,41 @@ app.post('/login',function(req,res){
     } ); 
 }); 
 
+ 
+app.get('/updatepatient', function(req,res){
+  res.render('users/simulate_wifi'); 
+});
+
+app.put('/updatepatient', function(req,res){
+var b = req.body ; 
+console.log(b.fall);
+console.log(b.alarm); 
+patientInfo.update(
+{
+  patientId: b.patientID
+}, 
+{
+  $push:
+  {
+    pulse: b.pulse,
+    oxygen: b.oxygen,
+    temp: b.temperature
+
+  }
+},{upsert: true},  function(err, data)
+{
+  console.log(data); 
+  res.redirect('/updatepatient'); 
+  
+}
+);
+}); 
+
+
+
+/*Message.update({_id: '5064aae4154cb34d14000001' },
+         {$push: { 'sent-messages' : delivered }},{upsert:true}, function(err, data) { 
+});*/
 
 
 //The signup button generates a POST request for /users. 
